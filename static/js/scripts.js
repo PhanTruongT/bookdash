d173f4d3ce7d9d0f768347496672376328ba495f
PhanTruongT <bplhp18@gmail.com>
2020-04-09 19:42:42 -0400

    added clear button

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,6 +7,18 @@ function openNav() {
 
 function closeNav() {
     document.getElementById("side-bar-expand").style.width = "0px";
-    
-   
+}
+
+function checkClearButton() {
+    if(document.getElementById("search-form-input").value.length > 0)
+    {
+        document.getElementById("clear-button").style.visibility = "visible";
+        document.getElementById("search-button").style.background = "#4d4d4d";
+    }
+    else
+    {
+        document.getElementById("clear-button").style.visibility = "hidden";
+       
+    }
+
 }
