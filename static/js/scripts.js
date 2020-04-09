457fc3d91dcb8acd9627fe4f160911d74357197e
PhanTruongT <bplhp18@gmail.com>
2020-04-09 17:44:30 -0400

    added Sign In box

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,12 @@
 function openNav() {
     document.getElementById("side-bar-expand").style.width = "180px";
-    document.getElementById("close-button").style.visibility = "visible";
-    document.getElementById("close-button-wrap").style.visibility = "visible";
+    
+    
+    
 }
 
 function closeNav() {
     document.getElementById("side-bar-expand").style.width = "0px";
-    document.getElementById("close-button").style.visibility = "hidden";
-    document.getElementById("close-button-wrap").style.visibility = "hidden";
+    
+   
 }
