90a760ace46bf3ff49921db354cfdaf652fb64fb
PhanTruongT <bplhp18@gmail.com>
2020-04-09 20:19:57 -0400

    added functionality to clear button

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -14,11 +14,37 @@ function checkClearButton() {
     {
         document.getElementById("clear-button").style.visibility = "visible";
         document.getElementById("search-button").style.background = "#4d4d4d";
+        return true;
     }
     else
     {
         document.getElementById("clear-button").style.visibility = "hidden";
-       
+        return false;
     }
 
+}
+
+function clearSearchInput() {
+    document.getElementById("search-form").reset();
+    document.getElementById("search-form-input").focus();
+    checkClearButton();
+
+}
+
+
+
+
+
+
+//set search-button highlight based on mouse hover and visibility state of clear button
+function setHighlight(value) {
+
+    if(value == 0 && !checkClearButton())
+    {
+        document.getElementById("search-button").style.background = "none";
+    }
+    else
+    {
+        document.getElementById("search-button").style.background = "#4d4d4d";
+    }
 }
